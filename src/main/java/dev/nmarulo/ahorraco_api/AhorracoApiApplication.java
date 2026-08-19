a26d2c3585ef21d92dae3e5c3f5dd48a5f63d902
Nicolás Marulanda P <nmarulo@outlook.com>
2026-08-19 15:17:12 +0200

    Init commit

---
+++ src/main/java/dev/nmarulo/ahorraco_api/AhorracoApiApplication.java
@@ -0,0 +1,13 @@
+package dev.nmarulo.ahorraco_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhorracoApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AhorracoApiApplication.class, args);
+    }
+    
+}
